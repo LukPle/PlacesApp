8521db4090dcc7be4cbb34e8b0d83509f431c217
Lukas Plenk <lukas.plenk@campus.lmu.de>
2024-04-02 17:48:59 +0200

    Initial Commit

---
+++ Places/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Places
+//
+//  Created by Lukas Plenk on 02.04.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Places/PlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesApp.swift
+//  Places
+//
+//  Created by Lukas Plenk on 02.04.24.
+//
+
+import SwiftUI
+
+@main
+struct PlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
