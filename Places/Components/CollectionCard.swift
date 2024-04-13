b1ab01ce36dcca93dc26faeabbf9b62b51fe1c8d
Lukas Plenk <lukasplenk@yahoo.de>
2024-04-13 22:31:25 +0200

    Added icon button, info pill, collection model, collection card and bottom sheet

---
+++ Places/Components/CollectionCard.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionCard.swift
+//  Places
+//
+//  Created by Lukas Plenk on 13.04.24.
+//
+
+import SwiftUI
+
+struct CollectionCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CollectionCard()
+}
---
+++ Places/Models/CollectionModel.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Places
+//
+//  Created by Lukas Plenk on 13.04.24.
+//
+
+import Foundation
